45da70ee6a4d8d046a5aadfb7368adffc5d2a559
TeaReaperr <Xavier.bischoff1604@gmail.com>
2023-09-20 14:34:05 +0200

    Altklausur Aufgabe 10 und 11 beendet

--- Baum.hs
+++ Baum.hs
@@ -1,7 +1,10 @@
 data Tree a b = Leaf b
-    | Branch (Tree a b) a (Tree a b)
+    | Branch (Tree a b) a (Tree a b) deriving Show
 
 getLeaves :: Tree a b -> [b]
 getLeaves (Leaf b) = [b]
 getLeaves (Branch x a y) = getLeaves x ++ getLeaves y
 
+bimap :: (a -> a') -> (b -> b') -> Tree a b -> Tree a' b'
+bimap f g (Leaf x) = Leaf(g x)
+bimap f g (Branch x a y) = Branch (bimap f g x) (f a) (bimap f g y)
